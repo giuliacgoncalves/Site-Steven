5297b178fac828a664049ce3851e233f9627e07e
Giulia Gonçalves <91676330+giuliacgoncalves@users.noreply.github.com>
2023-05-11 21:35:14 -0400

    Adicionando Menu

--- JS/index.js
+++ JS/index.js
@@ -26,16 +26,22 @@ function logar(){
     acessarHomePage();
 }
 
+/* ↓ Funçaõ para acessar a página principal ↓ */
+
 function acessarHomePage(){
     btnLogin.addEventListener('click', ()=>{
         location.href = '../Pages/home-page.html'
     })
 }
 
+/* ↓ Ir para página de Registro ↓ */
+
 const btnSignUp = document.querySelector("#buttonSignUp");
 btnSignUp.addEventListener('click', ()=>{
     location.href = '../Pages/register.html'
 })
+
+/* ↓ menu ↓ */
 
 var themeSteven = "../Style/styleIndex.css";
 var themeAmetista = "../Style/styleIndexAmetista.css";
@@ -43,42 +49,40 @@ var themePearl = "../Style/styleIndexPearl.css";
 var themeGarnet = "../Style/styleIndexGarnet.css";
 var themeLapis = "../Style/styleIndexLapis.css";
 var gemButtons = document.querySelectorAll(".gemBtn");
-// console.log(gemButtons)
 
 gemButtons.forEach(gemBtn => {
     gemBtn.addEventListener('click', trocarTema);
 })
 
+/* ↓ Função de troca do tema ↓ */
 
 function trocarTema(evt){
-    let alvoClicado = evt.target
-    let idAlvoClicado = evt.target.getAttribute('id')
-    let caminho = document.querySelector(".caminhoCss")
-    let temaAtual = 'sda'
+    let alvoClicado = evt.target;
+    let idAlvoClicado = evt.target.getAttribute('id');
+    let caminho = document.querySelector(".caminhoCss");
+    let temaAtual = '...'
+    // console.log(temaAtual);
 
-    if(idAlvoClicado == 'steven' && temaAtual != 'steven') {
-        console.log("cliquei no steven")
+    if(idAlvoClicado == 'steven') {
+        console.log("cliquei no steven");
         // alvoClicado.classList.add("selecionado")
-        caminho.href = themeSteven
+        caminho.href = themeSteven;
     }
-    else if(idAlvoClicado == 'pearl' && temaAtual != 'pearl') {
-        console.log("cliquei na pearl")
-        caminho.href = themePearl
+    else if(idAlvoClicado == 'pearl') {
+        console.log("cliquei na pearl");
+        caminho.href = themePearl;
     }
-    else if(idAlvoClicado == 'ametista' && temaAtual != 'ametista') {
-        console.log("cliquei na ametista")
-        caminho.href = themeAmetista
+    else if(idAlvoClicado == 'ametista') {
+        console.log("cliquei na ametista");
+        caminho.href = themeAmetista;
     }
-    else if(idAlvoClicado == 'garnet' && temaAtual != 'garnet') {
-        console.log("cliquei na garnet")
-        caminho.href = themeGarnet
+    else if(idAlvoClicado == 'garnet') {
+        console.log("cliquei na garnet");
+        caminho.href = themeGarnet;
     }
-    else if(idAlvoClicado == 'lapis' && temaAtual != 'lapis') {
-        console.log("cliquei na lapis")
-        caminho.href = themeLapis
+    else if(idAlvoClicado == 'lapis') {
+        console.log("cliquei na lapis");
+        caminho.href = themeLapis;
     }
-
-
-    // document.querySelector(".caminhoCss").href = themeSteven;
-    console.log(caminho);
+    console.log(caminho)
 }
